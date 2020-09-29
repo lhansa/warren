5417022b91499fb96bca85e5ab35426e58ec6f40
Leonardo Hansa <hansa.leonardo@gmail.com>
2020-09-29 22:46:13 +0200

    Dict and new symbols

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,20 @@
 import pandas as pd
 from yahoofinancials import YahooFinancials
 import matplotlib.pyplot as plt
+from datetime import date
 
 #%% Funciones guays
-def prep_df(simbolo):
+def prep_df(simbolo, valores, start='2020-01-01'):
     yf= YahooFinancials(simbolo)
     data= yf.get_historical_price_data(
-        start_date='2019-01-01',
-        end_date='2020-08-31', 
+        start_date=start,
+        end_date=date.today().strftime('%Y-%m-%d'), 
         time_interval='daily'
     ) 
  
     df= pd.DataFrame(data[simbolo]['prices'])
-    df['symb'] = simbolo
-    df= df.drop('date', axis=1).set_index(['symb', 'formatted_date'])
+    df['valor'] = valores
+    df= df.drop('date', axis=1).set_index(['valor', 'formatted_date'])
 
     return df
 
@@ -22,13 +23,21 @@ def prep_df(simbolo):
 # Amadeus AMS.MC
 # Yonghui 601933.SS
 
-simbolos = ['AMS.MC', '601933.SS', 'CSCO']
+valores = {'Amadeus':'AMS.MC', 
+           'Yonghui':'601933.SS', 
+           'Cisco':'CSCO', 
+           'Lenovo':'0992.HK'}
+#%% Preparación de parámetros
+simbolos = list(valores.values())
+l_valores = list(valores.keys())
+fecha_ini = ['2020-06-01'] * len(simbolos)
 
-l_dfs = list(map(prep_df, simbolos))
+#%% Descarga de datos
+l_dfs = list(map(prep_df, simbolos, l_valores, fecha_ini))
 
 #%% Agregación
 df_todo = pd.concat(l_dfs)
-df_todo_wide = df_todo.reset_index(drop=False).pivot(index='formatted_date', columns='symb', values='close')
+df_todo_wide = df_todo.reset_index(drop=False).pivot(index='formatted_date', columns='valor', values='close')
 
 #%% Visualización
-df_todo_wide.plot(subplots=True, ylim=(0,None))
+df_todo_wide.plot(subplots=True, ylim=(None,None))
